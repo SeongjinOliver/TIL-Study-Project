2209850e2d9f9807777f8eb32e16a5cb82916261
Seongjin Yoo <yoosngj@gmail.com>
2020-10-26 18:07:50 +0900

    Add creation of events test

---
+++ rest-api-with-spring/src/test/java/me/oliver/demoinflearnrestapi/events/EventControllerTests.java
@@ -0,0 +1,38 @@
+package me.oliver.demoinflearnrestapi.events;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+/**
+ * {@literal @WebMvcTest} 웹과 관련된 빈들이 모두 등록이 되고 테스트에서 MockMvc를 주입받아서 쉽게 사용 가능 MockMvc을 만들면 Mocking 되어
+ * 있는 DispatcherServlet을 상대로 가짜 요청을 만들어서 DispatcherServlet을 보내고 응답을 확인할 수 있는 Test를 만들 수 있음
+ * 웹과 관련된것만 등록을 해주기 때문에 slicing test라고 부름. 즉 모든 빈들을 등록해서 만드는 것이 아니라 웹과 관련된 빈들만 만들어서 사용하기 때문에
+ * Test가 빠름, 하지만 이 자체로 단위 테스트다라고 보기에는 어려움, 또한 DispatcherServlet, EventControll 이러한 것들이 포함되어서 테스트하는것이라
+ * 단위 테스트라고 보기에는 어려움. ---> 왜 사용하느냐 하면 여러가지 테스트를 편리하게 작성해서 사용할 수 있음
+ * MockMvc 는 웹 서버를 띄우지 않기 때문에 조금 더 빠르고, 하지만 DispatcherServlet을 만들기 때문에 단위 테스트보다 빠르지는 않음
+ */
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class EventControllerTests {
+
+  @Autowired
+  MockMvc mockMvc;
+
+  @Test
+  public void creatEvent() throws Exception {
+    mockMvc.perform(post("/api/events/")
+    .contentType(MediaType.APPLICATION_JSON)
+    .accept(MediaTypes.HAL_JSON)
+    )
+        .andExpect(status().isCreated());
+  }
+}
