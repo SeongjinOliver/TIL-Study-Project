8bf6cf772300aaa764e75f0e6cbaba13f9c6c9ea
Seongjin Yoo <yoosngj@gmail.com>
2020-10-26 17:19:45 +0900

    Made Event domain

--- rest-api-with-spring/src/main/java/me/oliver/demoinflearnrestapi/events/Event.java
+++ rest-api-with-spring/src/main/java/me/oliver/demoinflearnrestapi/events/Event.java
@@ -1,13 +1,28 @@
 package me.oliver.demoinflearnrestapi.events;
 
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
- * @{literal @Builder} Builder annotation을 사용 하면 컴파일 시점에 Builder 관련 코드를 실행 시킴
- * lombok : compile 시점에 어노테이션으로 특정 코드를 추가할 수 있게 도와 주는 java library
+ * @{literal @Builder} Builder annotation을 사용 하면 컴파일 시점에 Builder 관련 코드를 실행 시킴 lombok : compile 시점에
+ * 어노테이션으로 특정 코드를 추가할 수 있게 도와 주는 java library
+ * @{literal @EqualsAndHashCode} of="id"라고 준 이유는 equals와 haashcode를 구현할 때 모든 아래의 필드(변수)를 사용 나중에 객체간의
+ * 연관관계가 있을 때 서로 상호 참조 하는 관계가 되면 equals와 hashcode 구현한 코드 안에서 stckOverFlow가 발생 할 수 있음, 그리고 다른
+ * entity와의 연관관계를 만들면 좋지 않음 -> 서로간의 매서드를 호출하다가 stackOverFlow가 발생할 수 있음
+ *
+ * lombok 에노테이션은 메타 에노테이션으로 사용할 수 없음
  */
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
 public class Event {
 
   private int id;
