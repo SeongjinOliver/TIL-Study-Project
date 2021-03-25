97bf2546a00c787f6207cefea72ed460dcabf740
Seongjin Yoo <yoosngj@gmail.com>
2021-03-25 20:02:17 +0900

    Study Java 8 - Study Arrays.parallelSort

---
+++ java8to11/src/main/java/me/oliver/java8to11/ArrayParallelSort/ArrayParallelSortTest.java
@@ -0,0 +1,24 @@
+package me.oliver.java8to11.ArrayParallelSort;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.stream.IntStream;
+
+public class ArrayParallelSortTest {
+
+  public static void main(String[] args) {
+    int size = 1500;
+    int[] numbers = new int[size];
+    Random random = new Random();
+
+    IntStream.range(0, size).forEach(i -> numbers[i] = random.nextInt());
+    long start = System.nanoTime();
+    Arrays.sort(numbers);
+    System.out.println("serial sorting took " + (System.nanoTime() - start));
+
+    IntStream.range(0, size).forEach(i -> numbers[i] = random.nextInt());
+    start = System.nanoTime();
+    Arrays.parallelSort(numbers);
+    System.out.println("parallel sorting took " + (System.nanoTime() - start));
+  }
+}
