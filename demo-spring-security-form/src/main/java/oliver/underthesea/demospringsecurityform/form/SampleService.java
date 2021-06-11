4c24b995bcd40b83546460c5bec4b3f3108ddef3
Seongjin Yoo <yoosngj@gmail.com>
2021-06-12 00:27:44 +0900

    Add Authentication in SecurityContextHolder

---
+++ demo-spring-security-form/src/main/java/oliver/underthesea/demospringsecurityform/form/SampleService.java
@@ -0,0 +1,23 @@
+package oliver.underthesea.demospringsecurityform.form;
+
+import java.util.Collection;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+/**
+ * 사용자의 권한을 여러개 갖을 수 있음
+ * Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+ */
+@Service
+public class SampleService {
+
+  public void dashboard() {
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    Object principal = authentication.getPrincipal();
+    Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+    Object credentials = authentication.getCredentials();
+    boolean authenticated = authentication.isAuthenticated();
+  }
+}
