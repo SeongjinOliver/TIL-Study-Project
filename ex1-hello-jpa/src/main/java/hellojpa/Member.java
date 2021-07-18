6e2f8f40b3a542ddceed5b5e0c6f81a797bdd0c0
Seongjin Yoo <yoosngj@gmail.com>
2021-07-18 23:29:57 +0900

    Add TableGenerator annotation

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -5,17 +5,20 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
+import javax.persistence.TableGenerator;
 
 @Entity
-@SequenceGenerator(name = "MEMBER_SEQ_GENERATOR", sequenceName = "MEMBER_SEQ")
+@TableGenerator(
+    name = "MEMBER_SEQ_GENERATOR",
+    table = "MY_SEQUENCES",
+    pkColumnValue = "MEMBER_SEQ", allocationSize = 1)
 public class Member {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBER_SEQ_GENERATOR")
+  @GeneratedValue(strategy = GenerationType.TABLE, generator = "MEMBER_SEQ_GENERATOR")
   private Long id;
 
-  @Column(name = "name", nullable= true)
+  @Column(name = "name", nullable = false)
   private String username;
 
   public Member() {
