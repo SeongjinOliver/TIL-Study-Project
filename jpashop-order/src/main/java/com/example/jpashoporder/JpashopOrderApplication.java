93bbaa8593018c7e920131345daec4e1d669748f
SeongjinIllunex <sngj.yoo@illunex.com>
2022-10-06 23:12:26 +0900

    Add jpashop-order

---
+++ jpashop-order/src/main/java/com/example/jpashoporder/JpashopOrderApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpashoporder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpashopOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpashopOrderApplication.class, args);
+    }
+
+}
---
+++ jpashop-order/src/test/java/com/example/jpashoporder/JpashopOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpashoporder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpashopOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
