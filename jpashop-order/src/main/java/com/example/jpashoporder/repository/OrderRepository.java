c2a7e9cb2229358c93ff5278cd1b3f44d8b91d60
SeongjinIllunex <yoosngj@gmail.com>
2022-11-18 14:39:13 +0900

    Study jpa

--- jpashop-order/src/main/java/com/example/jpashoporder/repository/OrderRepository.java
+++ jpashop-order/src/main/java/com/example/jpashoporder/repository/OrderRepository.java
@@ -3,6 +3,7 @@ package com.example.jpashoporder.repository;
 import com.example.jpashoporder.domain.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
 import java.util.List;
@@ -21,5 +22,34 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-//    public List<Order> findAll(OrderSearch orderSearch){}
+    public List<Order> findAll(OrderSearch orderSearch) {
+        String jpql = "select o from Order o join o.member m";
+        boolean isFirstCondition = true;
+
+        //주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " o.status = :status";
+        }
+
+        //회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " m.name like :name";
+        }
+
+        return em.createQuery(jpql, Order.class)
+                .setMaxResults(1000) //최대 1000건
+                .getResultList();
+    }
 }
