ccfee84d45e4f85c452c45e6cf593b59c89427b1
SeongjinIllunex <yoosngj@gmail.com>
2022-11-23 00:05:05 +0900

    Study jpa

--- jpashop-order/src/main/java/com/example/jpashoporder/controller/MemberController.java
+++ jpashop-order/src/main/java/com/example/jpashoporder/controller/MemberController.java
@@ -8,9 +8,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -39,5 +41,12 @@ public class MemberController {
 
         memberService.join(member);
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
